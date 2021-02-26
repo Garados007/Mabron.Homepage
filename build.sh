6d47c1bd5231c1fe191f4578d44b4bf11ae13f30
Max Brauer <ma.brauer@live.de>
2021-02-26 13:40:39 +0100

    build markdown files

--- build.sh
+++ build.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# install deps
+sudo apt-get install -y markdown
+
+# build markdown
+for i in `find . -name "*.md" -type f`; do
+    markdown \
+        -f links,image,smarty,html,ext,cdata,superscript,emphasis,del,toc,divquote,alphalist,definitionlist,footnote \
+        -o "$(basename \"$i\" .md).html" \
+        "$i"
+done
+
 # copy files to output
 mkdir publish
 cp -r de publish/
